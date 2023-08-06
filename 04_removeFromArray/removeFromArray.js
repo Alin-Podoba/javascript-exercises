35c591a0d29ccbca5339d1891be600a2cbeee147
Alin-Podoba <Alinapodoba@gmail.com>
2023-08-06 16:30:35 +0300

    modified:   01_helloWorld/helloWorld.js 	modified:   02_repeatString/repeatString.js 	modified:   02_repeatString/repeatString.spec.js 	modified:   03_reverseString/reverseString.js 	modified:   03_reverseString/reverseString.spec.js 	modified:   04_removeFromArray/removeFromArray.js 	modified:   04_removeFromArray/removeFromArray.spec.js 	modified:   package-lock.json

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
 const removeFromArray = function() {
+    const arr = arguments[0];  // The first argument is the array
+    const valuesToRemove = Array.prototype.slice.call(arguments, 1);  // All other arguments are the values to remove
 
+    return arr.filter(item => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
